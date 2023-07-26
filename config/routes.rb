29216737e39d0f13bb0dd2e9153745ca02c9779a
Joyce Hong <jkim2027@gmail.com>
2023-07-26 10:11:28 -0400

    create user controller and template

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   
   root "movies#index"
-
+  
   resources :movies do
     resources :reviews
   end
+  
+  
+  resources :users
+  get "signup" => "users#new"
   
 end
